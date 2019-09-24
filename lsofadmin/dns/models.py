18181cad1fa20d98e11facc23b16731d881ebf53
Alex Conrey <alex@conrey.us>
2019-09-23 20:59:49 -0700

    added DomainManager

--- lsofadmin/dns/models.py
+++ lsofadmin/dns/models.py
@@ -2,15 +2,27 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
+class DomainManager(models.Model):
+    def create_domain(self, name):
+        domain = self.create(name=name)
+
+        return domain
+
 class Domain(models.Model):
     name = models.CharField(max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return 'Domain: {0}'.format(self.name)
-    
+
+    objects = DomainManager()
+
     class Meta:
         ordering = ['name']
+        managed = True
+
+        def __unicode__(self):
+            return self.name
 
 class Record(models.Model):
     name = models.CharField(max_length=255)
